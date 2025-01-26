6e1ae0a4f88697aedbb40fe307b712af09b60d76
Sandeep111555 <sandeepgautam111555@gmail.com>
2025-01-26 18:55:23 +0530

    added complex queries for fetching flight with airplanes,airports,city details

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,13 +1,47 @@
 const CrudRepository = require('./crud-repository');
-const { Flight } = require('../models');
+const { Flight, Airplane, Airport, City } = require('../models');
+const {Sequelize} = require('sequelize');
 
 class FlightRepository extends CrudRepository {
     constructor() {
         super(Flight);
     }
     //findAll function
-    async findAll(filters,sortFilter) {
-        const response = await this.model.findAll({ where: filters, order: sortFilter });
+    async findAll(filters, sortFilter) {
+        const response = await this.model.findAll({
+            where: filters,
+            order: sortFilter,
+            include: [{
+                model: Airplane,
+                as: 'airplaneDetail',
+                required: true,
+            },
+            {
+                model: Airport,
+                as: 'departureAirport',
+                on: {
+                    col1: Sequelize.where(Sequelize.col('departureAirportId'), '=', Sequelize.col('departureAirport.code'))
+                },
+                include: [{
+                    model: City,
+                    required: true,
+                }],
+                required: true,
+            },
+            {
+                model: Airport,
+                as: 'arrivalAirport',
+                on: {
+                    col1: Sequelize.where(Sequelize.col('arrivalAirportId'), '=', Sequelize.col('arrivalAirport.code'))
+                },
+                include: [{
+                    model: City,
+                    required: true,
+                }],
+                required: true,
+            },
+            ]
+        });
         return response;
     }
 };
